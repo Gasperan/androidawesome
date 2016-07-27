cd9651b11e693fb431a6d155d08a4e23e3b085e9
gaspar <gasoto@nisum.com>
2016-07-27 13:06:02 -0300

    added butterknife and material design libraries

--- app/src/main/java/nisum/com/parispilot/MainActivity.java
+++ app/src/main/java/nisum/com/parispilot/MainActivity.java
@@ -2,12 +2,24 @@ package nisum.com.parispilot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // uso de butterknife para linkear la vista con la logica
+    @BindView(R.id.textView) TextView mTextView;
+    @BindView(R.id.toolbar)  Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
     }
 }
