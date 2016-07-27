49a6e8cdcb225ea49bf921b88b8d7b7ca461d4aa
gaspar <gasoto@nisum.com>
2016-07-27 14:57:42 -0300

    added main layout

--- app/src/main/java/nisum/com/parispilot/MainActivity.java
+++ app/src/main/java/nisum/com/parispilot/MainActivity.java
@@ -1,25 +1,43 @@
 package nisum.com.parispilot;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     // uso de butterknife para linkear la vista con la logica
     @BindView(R.id.textView) TextView mTextView;
-    @BindView(R.id.toolbar)  Toolbar mToolbar;
+    @BindView(R.id.barcodeButton) ImageView mbarcodeButton;
+    @BindView(R.id.qrcodeButton) ImageView mQRCodeButton;
+    @BindView(R.id.manualbutton) ImageView mManualInputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-        setSupportActionBar(mToolbar);
+    }
+
+    @OnClick(R.id.barcodeButton)
+    public void onClickBarcode(View view) {
+        Toast.makeText(this, "barcode button", Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.qrcodeButton)
+    public void onClickQRCode(View view) {
+        Toast.makeText(this, "qrcode button", Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.manualbutton)
+    public void onClickManualInputButton(View view) {
+        Toast.makeText(this, "manual button", Toast.LENGTH_LONG).show();
     }
 }
