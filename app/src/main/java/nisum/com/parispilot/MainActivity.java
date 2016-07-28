65a0fef2858f540f1be8e7148e073aba02351074
gaspar <gasoto@nisum.com>
2016-07-28 16:31:07 -0300

    deleted splash activity

--- app/src/main/java/nisum/com/parispilot/MainActivity.java
+++ app/src/main/java/nisum/com/parispilot/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = INITIAL_REQUEST + 1;
 
     private static boolean splashLoaded = false;
-    private final int splashSeconds = 4000;
+    private final int splashSeconds = 3000;
 
     @BindView(R.id.textView) TextView mTextView;
     @BindView(R.id.barcodeButton) ImageView mbarcodeButton;
