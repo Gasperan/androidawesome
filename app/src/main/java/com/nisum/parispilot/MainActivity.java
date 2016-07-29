cdc6e8ee06248b3cc37a34a979c5e66e1d9ca927
gaspar <gasoto@nisum.com>
2016-07-29 17:29:28 -0300

    added changes

--- app/src/main/java/com/nisum/parispilot/MainActivity.java
+++ app/src/main/java/com/nisum/parispilot/MainActivity.java
@@ -1,8 +1,19 @@
 package com.nisum.parispilot;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
 import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
@@ -10,12 +21,27 @@ import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
 
 public class MainActivity extends AppCompatActivity{
 
-    //private Button mButtonShow;
+
+    private static final String[] CAMERA_PERMS = {
+            Manifest.permission.CAMERA
+    };
+
+    private static final int INITIAL_REQUEST = 1337;
+    private static final int CAMERA_REQUEST = INITIAL_REQUEST + 1;
+    private static final String SHOWCASE_ID = "main_showcase";
+
+    private static boolean splashLoaded = false;
+    private final int splashSeconds = 1000;
+
+    //fields
     private ImageView mBarcodeButton;
     private ImageView mQRButton;
     private ImageView mManualbutton;
 
-    private static final String SHOWCASE_ID = "main_showcase";
+    //dialog's fields
+    String tittleDialog;
+    String bodyDialog;
+    String buttonDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +51,34 @@ public class MainActivity extends AppCompatActivity{
         mBarcodeButton = (ImageView) findViewById(R.id.barcodeButton);
         mQRButton = (ImageView) findViewById(R.id.qrcodeButton);
         mManualbutton = (ImageView) findViewById(R.id.manualbutton);
+
+        tittleDialog = getResources().getString(R.string.dialog_title);
+        bodyDialog = getResources().getString(R.string.dialog_body);
+        buttonDialog = getResources().getString(R.string.dialog_button);
+
+        if (!splashLoaded) {
+            setContentView(R.layout.splash);
+            splashLoaded = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    setContentView(R.layout.activity_main);
+                    if (!canAccessCamera()) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(CAMERA_PERMS, CAMERA_REQUEST);
+                        }
+                    }
+                }
+            }, splashSeconds);
+
+        } else {
+            setContentView(R.layout.activity_main);
+            if (!canAccessCamera()) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(CAMERA_PERMS, CAMERA_REQUEST);
+                }
+            }
+        }
 
         presentShowcaseView(300); // one second delay
     }
@@ -47,6 +101,62 @@ public class MainActivity extends AppCompatActivity{
                 "Además, si ninguno de los anteriores te ha funcionado, puedes intentar ingresar el codigo manualmente", "GOT IT");
 
         sequence.start();
+    }
+
+
+    protected void showInputDialog() {
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(MainActivity.this, editText.getText(), Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
+    private boolean canAccessCamera() {
+        return (hasPermission(Manifest.permission.CAMERA));
+    }
+
+    private boolean hasPermission(String perm) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return (PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(tittleDialog)
+                .setMessage(bodyDialog)
+                .setPositiveButton(buttonDialog, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent surveyIntent = new Intent(MainActivity.this, SurveyActivity.class);
+                        startActivity(surveyIntent);
+                    }
+                }).show();
     }
 
 
