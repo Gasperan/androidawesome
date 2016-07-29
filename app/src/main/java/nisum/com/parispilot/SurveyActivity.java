f918c5c1fa75f976a50555ddb405981f5d20e839
Thomas <t.mcphillips@outlook.com>
2016-07-29 17:39:33 -0400

    with button for going to main activity

--- app/src/main/java/nisum/com/parispilot/SurveyActivity.java
+++ app/src/main/java/nisum/com/parispilot/SurveyActivity.java
@@ -1,8 +1,10 @@
 package nisum.com.parispilot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -10,21 +12,38 @@ import android.webkit.WebViewClient;
 
 import butterknife.BindView;
 
-public class SurveyActivity extends AppCompatActivity {
+public class SurveyActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_survey);
-        String url_survey = getResources().getString(R.string.url_survey);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+        final String url_survey = getResources().getString(R.string.url_survey);
+        myWebView = (WebView) findViewById(R.id.webView);
         myWebView.setWebChromeClient(new WebChromeClient());
+        String string = "Se ha registrado tu respuesta.";
+        myWebView.findAllAsync(string); //works for API levels 16 and up
         myWebView.clearCache(true);
         myWebView.clearHistory();
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-
         myWebView.loadUrl(url_survey);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.backButton:
+                Intent mainIntent = new Intent(SurveyActivity.this, MainActivity.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                SurveyActivity.this.startActivity(mainIntent);
+                break;
+
+
+        }
     }
 
 
