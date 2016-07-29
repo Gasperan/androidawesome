c4415873b8cdcf4ddc876d665193a7c92dce2a52
gaspar <gasoto@nisum.com>
2016-07-29 17:22:32 -0300

    added showcase

--- app/src/main/java/com/nisum/parispilot/MainActivity.java
+++ app/src/main/java/com/nisum/parispilot/MainActivity.java
@@ -1,178 +1,53 @@
 package com.nisum.parispilot;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import com.nisum.parispilot.model.Barcode;
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
 
-public class MainActivity extends AppCompatActivity {
 
-    private static final String[] CAMERA_PERMS = {
-            Manifest.permission.CAMERA
-    };
+public class MainActivity extends AppCompatActivity{
 
-    private static final int INITIAL_REQUEST = 1337;
-    private static final int CAMERA_REQUEST = INITIAL_REQUEST + 1;
+    //private Button mButtonShow;
+    private ImageView mBarcodeButton;
+    private ImageView mQRButton;
+    private ImageView mManualbutton;
 
-    private static boolean splashLoaded = false;
-    private final int splashSeconds = 1000;
-
-    @BindView(R.id.textView) TextView mTextView;
-    @BindView(R.id.barcodeButton) ImageView mbarcodeButton;
-    @BindView(R.id.qrcodeButton) ImageView mQRCodeButton;
-    @BindView(R.id.manualbutton) ImageView mManualInputButton;
-    String tittleDialog;
-    String bodyDialog;
-    String buttonDialog;
+    private static final String SHOWCASE_ID = "main_showcase";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tittleDialog = getResources().getString(R.string.dialog_title);
-        bodyDialog = getResources().getString(R.string.dialog_body);
-        buttonDialog = getResources().getString(R.string.dialog_button);
-        if (!splashLoaded) {
-            setContentView(R.layout.splash);
-            splashLoaded = true;
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    setContentView(R.layout.activity_main);
-                    ButterKnife.bind(MainActivity.this);
-                    if (!canAccessCamera()) {
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            requestPermissions(CAMERA_PERMS, CAMERA_REQUEST);
-                        }
-                    }
-                }
-            }, splashSeconds);
 
-        } else {
-            setContentView(R.layout.activity_main);
-            ButterKnife.bind(MainActivity.this);
-            if (!canAccessCamera()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    requestPermissions(CAMERA_PERMS, CAMERA_REQUEST);
-                }
-            }
-        }
+        mBarcodeButton = (ImageView) findViewById(R.id.barcodeButton);
+        mQRButton = (ImageView) findViewById(R.id.qrcodeButton);
+        mManualbutton = (ImageView) findViewById(R.id.manualbutton);
+
+        presentShowcaseView(300); // one second delay
     }
 
-    @OnClick(R.id.barcodeButton)
-    public void onClickBarcode(View view) {
-        if (canAccessCamera()) {
-            startActivityForResult(new Intent(MainActivity.this, Barcode.class), 1);
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(CAMERA_PERMS, CAMERA_REQUEST);
-            }
-        }
+    private void presentShowcaseView(int withDelay) {
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(withDelay);
+
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this, SHOWCASE_ID);
+
+        sequence.setConfig(config);
+
+        sequence.addSequenceItem(mBarcodeButton,"Lector de codigo de barras",
+                "Aqui puedes escanear codigos de barra", "GOT IT");
+
+        sequence.addSequenceItem(mQRButton,"Lector de codigos QR",
+                "con este boton escaneas codigos QR", "GOT IT");
+
+        sequence.addSequenceItem(mManualbutton,"Escaner manual",
+                "Además, si ninguno de los anteriores te ha funcionado, puedes intentar ingresar el codigo manualmente", "GOT IT");
+
+        sequence.start();
     }
 
-    @OnClick(R.id.qrcodeButton)
-    public void onClickQRCode(View view) {
-        startActivityForResult(new Intent(MainActivity.this, Barcode.class), 1);
-    }
 
-    @OnClick(R.id.manualbutton)
-    public void onClickManualInputButton(View view) {
-        showInputDialog();
-    }
-
-    //this captures the result from barcode and populates in the searchView.
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 1) {
-            if (data != null) {
-                String barcode = data.getStringExtra("BARCODE");
-                if (!barcode.equals("NULL")) {
-                    Log.d("barcode", barcode);
-                    Toast.makeText(this, barcode, Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(this, ResultActivity.class);
-                    startActivity(intent);
-                    //search.setQuery(barcode, true);
-                    //search.setIconifiedByDefault(false);
-                }
-
-            }
-        }
-
-    }
-
-    protected void showInputDialog() {
-        // get prompts.xml view
-        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
-        View promptView = layoutInflater.inflate(R.layout.input_dialog, null);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-        alertDialogBuilder.setView(promptView);
-
-        final EditText editText = (EditText) promptView.findViewById(R.id.edittext);
-        // setup a dialog window
-        alertDialogBuilder.setCancelable(false)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Toast.makeText(MainActivity.this, editText.getText(), Toast.LENGTH_LONG).show();
-                        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                        startActivity(intent);
-                    }
-                })
-                .setNegativeButton("Cancel",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-        // create an alert dialog
-        AlertDialog alert = alertDialogBuilder.create();
-        alert.show();
-    }
-
-    private boolean canAccessCamera() {
-        return (hasPermission(Manifest.permission.CAMERA));
-    }
-
-    private boolean hasPermission(String perm) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return (PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
-        }
-        return true;
-    }
-    @Override
-    public void onBackPressed() {
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle(tittleDialog)
-                .setMessage(bodyDialog)
-                .setPositiveButton(buttonDialog, new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent surveyIntent = new Intent(MainActivity.this, SurveyActivity.class);
-                        startActivity(surveyIntent);
-                    }
-
-                })
-                .show();
-    }
 }
-
-
