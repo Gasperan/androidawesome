354cd442afea1bca6d9b16f8b598a7e4d47ae8cb
Claudio <Claudio_Gonzalez@gap.com>
2016-08-02 15:19:19 -0400

    test

--- app/src/main/java/nisum/com/parispilot/MainActivity.java
+++ app/src/main/java/nisum/com/parispilot/MainActivity.java
@@ -1,5 +1,6 @@
 package nisum.com.parispilot;
 
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -7,11 +8,17 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,38 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
+
+        presentShowcaseView(300);
+    }
+
+    private void presentShowcaseView(int withDelay) {
+
+        //ImageView imageView = (ImageView)findViewById(R.id.tile_picture);
+
+        //CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.main_content);
+
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(withDelay);
+        MaterialShowcaseSequence sequence = new MaterialShowcaseSequence(this);
+        sequence.setConfig(config);
+        //sequence.addSequenceItem(coordinatorLayout, "Scroll to move", "Descripcion", "OK");
+
+
+        /*
+        sequence.addSequenceItem(imageView, "Otra funcionalidad",
+                "Descripcion de la otra funcionalidad", "OK");
+
+        sequence.addSequenceItem(mBarcodeButton, "Lector de codigo de barras",
+                "Aqui puedes escanear codigos de barra", "OK");
+
+        sequence.addSequenceItem(mQRButton, "Lector de codigos QR",
+                "con este boton escaneas codigos QR", "OK");
+
+        sequence.addSequenceItem(mManualbutton, "Escaner manual",
+                "Además, si ninguno de los anteriores te ha funcionado, puedes intentar ingresar el codigo manualmente", "OK");
+        */
+
+        sequence.start();
     }
 
     // Add Fragments to Tabs
