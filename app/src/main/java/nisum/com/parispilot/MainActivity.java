48637a67d1203955a48e357c4a93e6b267882bc7
Claudio <Claudio_Gonzalez@gap.com>
2016-08-05 12:42:13 -0400

    Showcase only 1 time

--- app/src/main/java/nisum/com/parispilot/MainActivity.java
+++ app/src/main/java/nisum/com/parispilot/MainActivity.java
@@ -1,6 +1,7 @@
 package nisum.com.parispilot;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -33,6 +34,11 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,76 +52,12 @@ public class MainActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
-    }
+        sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
 
-    private static class CustomShowcaseView implements ShowcaseDrawer {
-
-        private final float width;
-        private final float height;
-        private final Paint eraserPaint;
-        private final Paint basicPaint;
-        private final int eraseColour;
-        private final RectF renderRect;
-
-        public CustomShowcaseView(Resources resources) {
-            width = resources.getDimension(R.dimen.custom_showcase_width);
-            height = resources.getDimension(R.dimen.custom_showcase_height);
-            PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY);
-            eraserPaint = new Paint();
-            eraserPaint.setColor(0xFFFFFF);
-            eraserPaint.setAlpha(0);
-            eraserPaint.setXfermode(xfermode);
-            eraserPaint.setAntiAlias(true);
-            eraseColour = resources.getColor(R.color.custom_showcase_bg);
-            basicPaint = new Paint();
-            renderRect = new RectF();
-        }
-
-        @Override
-        public void setShowcaseColour(int color) {
-            eraserPaint.setColor(color);
-        }
-
-        @Override
-        public void drawShowcase(Bitmap buffer, float x, float y, float scaleMultiplier) {
-            Canvas bufferCanvas = new Canvas(buffer);
-            renderRect.left = x - width / 2f;
-            renderRect.right = x + width / 2f;
-            renderRect.top = y - height / 2f;
-            renderRect.bottom = y + height / 2f;
-
-
-            bufferCanvas.drawRect(renderRect, eraserPaint);
-        }
-
-        @Override
-        public int getShowcaseWidth() {
-            return (int) width;
-        }
-
-        @Override
-        public int getShowcaseHeight() {
-            return (int) height;
-        }
-
-        @Override
-        public float getBlockedRadius() {
-            return width;
-        }
-
-        @Override
-        public void setBackgroundColour(int backgroundColor) {
-            // No-op, remove this from the API?
-        }
-
-        @Override
-        public void erase(Bitmap bitmapBuffer) {
-            bitmapBuffer.eraseColor(eraseColour);
-        }
-
-        @Override
-        public void drawToCanvas(Canvas canvas, Bitmap bitmapBuffer) {
-            canvas.drawBitmap(bitmapBuffer, 0, 0, basicPaint);
+        if(!sharedPreferences.getBoolean("showcaseLoaded",false)) {
+            editor = sharedPreferences.edit();
+            editor.putBoolean("showcaseLoaded", false);
+            editor.commit();
         }
 
     }
@@ -172,9 +114,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
-        presentShowcaseView();
+
+        if(!sharedPreferences.getBoolean("showcaseLoaded",false)){
+            editor.putBoolean("showcaseLoaded",true);
+            editor.commit();
+            presentShowcaseView();
+        }
+
         return true;
     }
 
